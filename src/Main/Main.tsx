3a69d411526fe5e7cd9cdf9053a5de812f1a148e
Elijah <xrealntrue16x@gmail.com>
2026-06-29 00:30:28 -0400

    Main CSS Edited

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -3,9 +3,10 @@ import "./Main.css";
 type MainProps = {};
 
 function Main({ }: MainProps) {
+
     return (
         <div className="main">
-            <h1 className="main__title">Jobs</h1>
+            <h1 className="main__title">Stats</h1>
             {/* Boxes */}
             <div className="main__container">
                 <div className="main__box">Saved</div>
