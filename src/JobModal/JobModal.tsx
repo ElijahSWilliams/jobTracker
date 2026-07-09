6686782ddab3549099b013970f0272a5121460db
Elijah <xrealntrue16x@gmail.com>
2026-07-08 23:11:55 -0400

    Working on Mobile Displays

--- src/JobModal/JobModal.tsx
+++ src/JobModal/JobModal.tsx
@@ -7,9 +7,17 @@ type JobModalProps = {
 };
 
 export default function JobModal({ job }: JobModalProps) {
-    console.log(job)
+
+
+
+
     return (
         <div className="job__modal">
+            <div className="job__modal-btns">
+                <button className="job__modal-edit-btn">Edit </button>
+                <button className="job__modal-delete-btn"> Delete</button>
+            </div>
+
             <h1>{job.company}</h1>
             <p>{job.position}</p>
             <p>Applied on: {job.dateAdded}</p>
--- src/addJobModal/AddJob.tsx
+++ src/addJobModal/AddJob.tsx
@@ -28,7 +28,6 @@ function AddJob({ toggleModalClose, addJob }: AddJobProps) {
             dateAdded: new Date().toLocaleDateString(),
         }
 
-
         addJob(newJob);
         toggleModalClose();
     }
@@ -37,10 +36,7 @@ function AddJob({ toggleModalClose, addJob }: AddJobProps) {
 
     return (
         <>
-            <div className="modal__header">
-                <h1 className="modal__title">Add Job</h1>
-                <button className="modal__close" onClick={toggleModalClose}> X </button>
-            </div>
+
             {/* FORM */}
             <form className="modal__form" onSubmit={handleSubmit}>
                 <input className="modal__form-input" type="text" placeholder="Company Name" value={formData.company} required onChange={(e) => {
