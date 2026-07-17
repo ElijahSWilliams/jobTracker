a4c6d985dac0f3424cc11d57507b781b8ac5fbbe
Elijah <xrealntrue16x@gmail.com>
2026-07-17 14:36:40 -0400

    Header Layout Done

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -2,15 +2,13 @@ import "./Header.css";
 
 type HeaderProps = {};
 
-function Header({ }: HeaderProps) {
-    return (
-        <div className="header">
-            <div className="header__nav">
-                <h1 className="header__title">Job Tracker</h1>
-                <button className="header__account-btn">Login /Logout</button>
-            </div>
-        </div>
-    );
+function Header({}: HeaderProps) {
+  return (
+    <div className="header">
+      <h1 className="header__title">Job Tracker</h1>
+      <button className="header__account-btn">Login /Logout</button>
+    </div>
+  );
 }
 
 export default Header;
