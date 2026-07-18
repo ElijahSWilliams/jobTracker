ad19935d99a0e5d484edea2024b71087a341ac8f
Elijah <xrealntrue16x@gmail.com>
2026-07-17 20:56:01 -0400

    Rough responsive design finished for small screens

--- src/Profile/Profile.tsx
+++ src/Profile/Profile.tsx
@@ -2,71 +2,68 @@ import { useState } from "react";
 import "./Profile.css";
 
 type Stat = {
-    label: string;
-    value: number;
-}
+  label: string;
+  value: number;
+};
 
 type ProfileProps = {
-    name: string; /* Username */
-    image: string; /* Profile pic */
-    open: boolean;
-    toggleSidebar: () => void;
+  name: string; /* Username */
+  image: string; /* Profile pic */
+  open: boolean;
+  toggleSidebar: () => void;
 
-    stats: Stat[];
+  stats: Stat[];
 };
 
 function Profile({ open, name, image, toggleSidebar, stats }: ProfileProps) {
-    const [profileImage, setProfileImage] = useState(image);
+  const [profileImage, setProfileImage] = useState(image);
 
-    /* FUNCTIONS */
-    function handleImageChange(event: React.ChangeEvent<HTMLInputElement>) {
-        const file = event.target.files?.[0]; //loof for files in event object and grab first one
+  /* FUNCTIONS */
+  function handleImageChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files?.[0]; //loof for files in event object and grab first one
 
-        if (!file) return; //if no file, exit
+    if (!file) return; //if no file, exit
 
-        const url = URL.createObjectURL(file); //create URL with react keywords??
+    const url = URL.createObjectURL(file); //create URL with react keywords??
 
-        setProfileImage(url); //set image to url
+    setProfileImage(url); //set image to url
+  }
 
-    }
+  return (
+    <aside className={`profile ${open ? "profile-open" : "profile-closed"}`}>
+      <button className="profile__toggle" onClick={toggleSidebar}>
+        {open ? "⬅" : "➡"}
+      </button>
+      <div className="profile__content">
+        <label className="profile__image-wrapper">
+          <img
+            src={image}
+            alt={`${name}'s Profile`}
+            className="profile__image"
+          />
+          <input
+            type="file"
+            accept="image/*"
+            hidden
+            onChange={handleImageChange}
+          />
+        </label>
 
+        {/* DISPLAY USER NAME */}
+        <h1 className="profile__name">Hello, {name}!</h1>
 
-
-    return (
-
-        <aside className={`profile ${open ? "profile-open" : "profile-closed"}`}>
-            <button className="profile__toggle" onClick={toggleSidebar}>
-                {open ? "⬅" : "➡"}
-            </button>
-            <div className="profile__content">
-
-                <label className="profile__image-wrapper">
-                    <img src={image} alt={`${name}'s Profile`} className="profile__image" />
-                    <input type="file" accept="image/*" hidden onChange={handleImageChange} />
-                </label>
-
-                {/* DISPLAY USER NAME */}
-                <h1 className="profile__name">
-                    Hello,  {name}!
-                </h1>
-
-
-                {/*  */}
-                <div className="profile__stats">
-                    {stats.map((stat) => (
-                        <div className="profile__stat" key={stat.label}>
-                            <p>{stat.label}</p>
-                            <p>{stat.value}</p>
-                        </div>
-                    ))}
-                </div> 
-
-                <button className="profile__logout" onClick={() => {
-                    alert("Logout Function in progress");
-                }}>Logout</button>
+        {/*  */}
+        <div className="profile__stats">
+          {stats.map((stat) => (
+            <div className="profile__stat" key={stat.label}>
+              <p>{stat.label}</p>
+              <p>{stat.value}</p>
             </div>
-        </aside>
-    );
+          ))}
+        </div>
+      </div>
+    </aside>
+  );
 }
 
 export default Profile;
