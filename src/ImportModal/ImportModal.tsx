d434dc506d0cb4a40a2d3399f986fb05a896f339
Elijah <xrealntrue16x@gmail.com>
2026-09-26 09:05:36 -0400

    Starting OAuth with Google Sheets API

--- src/ImportModal/ImportModal.tsx
+++ src/ImportModal/ImportModal.tsx
@@ -21,11 +21,12 @@ export default function ImportModal({ onClose }: ImportModalProps) {
 
     return (
         <div>
-            <form onSubmit={() => alert("Open")}>
+            <button className="" onClick={() => alert("OAuth")}></button>
+            {/*  <form onSubmit={() => alert("Open")}>
                 <input className="signup__modal-form-input" type="text" placeholder="email" required value={email} onChange={handleEmailChange}></input>
                 <input className="signup__modal-form-input" type="password" placeholder="password" required value={password} onChange={handlePasswordChange}></input>
-                <button type="submit">Import</button>
-            </form>
+                <button type="submit">Import with Google Sheets</button>
+            </form> */}
         </div>
     );
 }
